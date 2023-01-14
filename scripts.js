78a70103839698b75de897eb51369e76cbfae443
sinuCletus <84879210+sinuCletus@users.noreply.github.com>
2023-01-14 08:23:27 -0800

    Add math operation function that takes an operator and runs a math function
    A math operation function added that takes an operator and two operands.
    Based on the operator the function will call one of the math functions
    created earlier. This will be used later with the visual calculator
    when users press numbers and operators on it to do calculations.

--- scripts.js
+++ scripts.js
@@ -4,3 +4,11 @@ const sum = (a,b) => a + b;
 const differene = (a,b) => a - b;
 const product = (a,b) => a * b;
 const quotient = (a,b) => a / b;
+
+const operation = (operator, a, b) => 
+
+    (operator == '+') ? sum(a,b) 
+    : (operator == '-') ? differene(a,b)
+    : (operator == '*') ? product(a,b) 
+    : (operator == '/') ? quotient(a,b)
+    : console.log(NaN);
