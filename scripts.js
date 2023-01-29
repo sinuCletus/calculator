701a746e02042b313b91d0d5b9a78c7cc197be09
sinuCletus <84879210+sinuCletus@users.noreply.github.com>
2023-01-29 09:17:29 -0800

    Add functionality to calculator
    I used a switch function to set up my calculator button functions based on
    button attributes. This was to make sure that a user can enter a number,
    an operator, and another number, then hit = and the operation function
    should fire based on this information and correctly produce an answer.
    
    The answer is also displayed now in one of two divs. The calculation will
    be on the bottom div and when they hit = the answer will be above the
    equation.
    
    I could not figure out how to do negative numbers.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,93 @@
-//FUNCTIONS add, subtract, multiply, divide
-
 const sum = (a,b) => a + b;
-const differene = (a,b) => a - b;
+const difference = (a,b) => a - b;
 const product = (a,b) => a * b;
 const quotient = (a,b) => a / b;
+
+const total = window.document.getElementById('total');
+const current = window.document.getElementById('current');
+const buttons = Array.from(window.document.getElementsByTagName('button'));
+let operator = '';
+let operandA = '';
+let operandB = '';
+
+//FUNCTIONS add, subtract, multiply, divide, and operation
 
 const operation = (operator, a, b) => 
 
     (operator == '+') ? sum(a,b) 
-    : (operator == '-') ? differene(a,b)
-    : (operator == '*') ? product(a,b) 
+    : (operator == '-') ? difference(a,b)
+    : (operator == 'x') ? product(a,b) 
     : (operator == '/') ? quotient(a,b)
     : console.log(NaN);
+
+//-------------------------------------------------------
+
+//Display Functionality
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+       switch(e.target.innerText) {
+        //Clears entire calculator
+        case 'Clear': 
+            current.innerHTML = '';
+            total.innerHTML = '';
+            break;
+        //Deletes one number or operator at a time
+        case 'Delete': 
+            current.innerHTML = current.innerHTML.slice(0,-1);
+            break;
+        //Operators, only able to apply an operator once 
+        case '+':
+            operator = e.target.innerText;
+
+            if(!current.innerHTML.includes(operator) && !current.innerHTML.includes('x')
+            && !current.innerHTML.includes('-') && !current.innerHTML.includes('/')) {
+
+                current.innerText += e.target.innerText;
+            }
+            break;
+        case '-':
+            operator = e.target.innerText;
+
+            if(!current.innerHTML.includes(operator) && !current.innerHTML.includes('x')
+            && !current.innerHTML.includes('+') && !current.innerHTML.includes('/')) {
+               
+                current.innerText += operator;
+            }
+            break;
+        case 'x':
+            operator = e.target.innerText;
+
+            if(!current.innerHTML.includes(operator) && !current.innerHTML.includes('+')
+            && !current.innerHTML.includes('-') && !current.innerHTML.includes('/')) {
+               
+                current.innerText += e.target.innerText;
+            }
+            break;
+        case '/':
+            operator = '/';
+
+            if(!current.innerHTML.includes(operator) && !current.innerHTML.includes('x')
+            && !current.innerHTML.includes('-') && !current.innerHTML.includes('+')) {
+               
+                current.innerText += e.target.innerText;
+            }
+            break;
+        //Calls the operation function once = is pressed
+        case '=':
+            let operands = current.innerText.split(operator);
+            
+            operandA = operands[0];
+            operandB = operands[1];
+
+            current.innerHTML = ''
+
+            total.innerHTML = operation(operator, parseFloat(operandA), parseFloat(operandB));
+            break;
+        default:
+            current.innerText += e.target.innerText;
+       }
+    });
+});
+
+
